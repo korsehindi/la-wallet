a27500a29b8e469a06d8a90f17a1bf8c052242ec
Andrea Gómez <crypto.andrea@protonmail.ch>
2018-08-09 15:33:08 -0500

    Creating unique attribute values for Customers

--- api/spec/factories/customers.rb
+++ api/spec/factories/customers.rb
@@ -2,13 +2,13 @@ require 'faker'
 
 FactoryBot.define do
   factory :customer do
-    email Faker::Internet.unique.email
-    password_digest Faker::Internet.password
-    title Faker::Name.prefix
-    country Faker::Address.country_code
-    name Faker::Name.name
-    gender Faker::Gender.type
-    age Faker::Number.number(2)
-    slug Faker::Internet.slug
+    sequence(:email) { Faker::Internet.unique.email }
+    sequence(:slug) { Faker::Internet.unique.slug }
+    sequence(:password_digest) { Faker::Internet.unique.password }
+    sequence(:title) { Faker::Name.prefix }
+    sequence(:country) { Faker::Address.country_code }
+    sequence(:name) { Faker::Name.name }
+    sequence(:gender) { Faker::Gender.binary_type }
+    sequence(:age) { Faker::Number.number(2) }
   end
 end
