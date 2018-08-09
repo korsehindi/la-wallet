671c86c2a9c1472de873e773f482d8dba86a37cd
Andrea Gómez <crypto.andrea@protonmail.ch>
2018-08-09 15:54:00 -0500

    Added bcrypt as password hasher for customer factory

--- api/spec/factories/customers.rb
+++ api/spec/factories/customers.rb
@@ -1,10 +1,11 @@
+require 'bcrypt'
 require 'faker'
 
 FactoryBot.define do
   factory :customer do
     sequence(:email) { Faker::Internet.unique.email }
     sequence(:slug) { Faker::Internet.unique.slug }
-    sequence(:password_digest) { Faker::Internet.unique.password }
+    sequence(:password_digest) { BCrypt::Password.create(Faker::Internet.unique.password) }
     sequence(:title) { Faker::Name.prefix }
     sequence(:country) { Faker::Address.country_code }
     sequence(:name) { Faker::Name.name }
