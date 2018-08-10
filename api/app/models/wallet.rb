11c60e137c08297796734b2e9d64755cbf6b24f6
Andrea Gómez <crypto.andrea@protonmail.ch>
2018-08-10 18:07:03 -0500

    Wallet tests and validators

--- api/app/models/wallet.rb
+++ api/app/models/wallet.rb
@@ -1,3 +1,20 @@
 class Wallet < ApplicationRecord
+  # Constants
+  VALID_TEXT_REGEX = /\A[a-zA-Z]+\z/
   belongs_to :customer
+
+  # Mandatory attributes
+  validates :customer,
+            presence: true
+  validates :address,
+            presence: true,
+            uniqueness: true,
+            length: { is: 64 }
+  validates :balance,
+            presence: true,
+            numericality: { greater_than_or_equal_to: 0 }
+  validates :alias,
+            presence: false,
+            length: { maximum: 128 },
+            format: { with: VALID_TEXT_REGEX }
 end
