603aeb8d1e15a6d9a096e2aa20a6b4dbf8fcbb40
Andrea Gómez <crypto.andrea@protonmail.ch>
2018-08-09 21:34:21 -0500

    Added Customer seeds for our Customer DB

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Customer.create([
+                  { "email"=>"irwinwatsica@jones.biz",
+                    "password_digest"=>"28f719c89ef7f33ce2e178490676b5ab",
+                    "slug"=>"ex_assumenda",
+                    "name"=>"Troy Leuschke",
+                    "title"=>"Ms.",
+                    "gender"=>"Non-binary",
+                    "age"=>67,
+                    "country"=>"AD" },
+
+                  { "email"=>"myron@grant.biz",
+                    "password_digest"=>"ec2d4e2cdf77da89694ed921f6fa7a8c",
+                    "slug"=>"natus_sunt",
+                    "name"=>"Jonathan Howe",
+                    "title"=>"Mr.",
+                    "gender"=>"Male",
+                    "age"=>58,
+                    "country"=>"MS" },
+
+                  { "email"=>"sadie@borermarquardt.info",
+                    "password_digest"=>"d94016c07f86a27e30bef01e2b8409ac",
+                    "slug"=>"aliquam_fuga",
+                    "name"=>"Gale Miller",
+                    "title"=>"Mrs.",
+                    "gender"=>"Genderfluid",
+                    "age"=>17,
+                    "country"=>"TK" }
+                ])
