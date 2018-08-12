5c77331d785310c07e36ac7f5c5db8eb3787c20f
Andrea Gómez <crypto.andrea@protonmail.ch>
2018-08-12 18:33:20 -0500

    Deleted remaining files from the dir structure change

--- api/app/serializers/customer_serializer.rb
+++
@@ -1,3 +0,0 @@
-class CustomerSerializer < ActiveModel::Serializer
-  attributes :id, :name, :url, :avatar_url, :provider, :slug, :email, :location
-end
--- api/db/migrate/20180810055553_added_slug_to_customers.rb
+++
@@ -1,5 +0,0 @@
-class AddedSlugToCustomers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :customers, :slug, :string
-  end
-end
